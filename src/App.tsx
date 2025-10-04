e39248da233e6143d6e742556bf531413412edb4
Leticia Amen <letiamen23@gmail.com>
2025-10-04 13:39:15 -0300

    Fix: Add basename to BrowserRouter for GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/leticia-amen-portfolio-c60c95a0">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
